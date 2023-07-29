8cbcb4517dad14d2e77e2b58e78e0af1844aba37
vineeth <vineeth.jub@gmail.com>
2023-07-29 10:51:50 +0530

    added new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+
+function App() { 
+  const handleDownload = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/download-pdf', {
+
+      "filename":"okay"
+      },{
+        responseType: 'blob', // Set the response type to 'blob' for file downloads
+      });
+
+      // Create a Blob from the response data
+      const blob = new Blob([response.data], { type: 'application/pdf' });
+
+      // Create a URL for the Blob
+      const url = URL.createObjectURL(blob);
+
+      // Use an anchor element to initiate the download
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'new_pro.pdf'; // Specify the desired filename
+      a.click();
+
+      // Clean up the URL object to release resources
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error('Error downloading PDF:', error);
+    }
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <button onClick={handleDownload}>Download PDF</button>
     </div>
   );
 }
